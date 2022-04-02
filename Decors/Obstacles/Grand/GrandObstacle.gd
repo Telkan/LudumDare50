0fb564b4e79fe2707ea17993038b438be4c7b0d0
Telkan <gamba.gael@orange.fr>
2022-04-02 19:03:30 +0200

    Random Spawn des obstacles

--- Decors/Obstacles/Grand/GrandObstacle.gd
+++ Decors/Obstacles/Grand/GrandObstacle.gd
@@ -1,11 +1,10 @@
-extends Node2D
+extends "res://Scripts/ScrollMovement.gd"
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-export var runSpeed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,14 +13,14 @@ func _ready():
 	var chosen = rng.randi_range(1,2)
 	var texture = load("res://Decors/Obstacles/Grand/"+str(chosen)+".png")
 	texture.set_flags(2)
-	get_node("RigidBody2D/Sprite").set_texture(texture)
+	get_node("StaticBody2D/Sprite").set_texture(texture)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	get_node("RigidBody2D").set_linear_velocity(Vector2(-runSpeed*delta,0))
+	move(delta)
 	pass
 	
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+#func _on_VisibilityNotifier2D_screen_exited():
+#	queue_free()
--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -3,10 +3,13 @@ extends Node2D
 export (Array, PackedScene) var scenes
 
 var random_scene = RandomNumberGenerator.new()
+var random_timer = RandomNumberGenerator.new()
 var selected_scene_index = 0
 
 func _on_Timer_timeout():
 	random_scene.randomize()
-	selected_scene_index = random_scene.randi_range(0,scenes.size()-1)
+	random_timer.randomize()
+	get_node("Timer").set_wait_time(random_timer.randf_range(0,2))
+	selected_scene_index = random_scene.randi_range(0,1)
 	var tmp = scenes[selected_scene_index].instance()
 	add_child(tmp)
